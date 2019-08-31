8858b0b5df43da8ba3c7cd7d63c89af0dd0a94bc
meloveayu <2513367550@qq.com>
2019-08-31 16:51:26 +0800

    code reconfiguration 1st edition

--- app/src/main/java/com/example/numbergame/MainActivity.java
+++ app/src/main/java/com/example/numbergame/MainActivity.java
@@ -13,47 +13,48 @@ public class MainActivity extends AppCompatActivity {
     private Button mNumOneButton;
     private Button mNumTwoButton;
     private TextView mPointText;
-    public static int num = 0;
+    private static int num = 0;
+    private void randomSum(){
+        mNumOneButton.setText(String.valueOf((int) (Math.random() * 100)));
+        mNumTwoButton.setText(String.valueOf((int) (Math.random() * 100)));
+    }
+    private void toastShow(int n_1,int n_2){
+        if( n_1 >= n_2){
+            Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+            mPointText.setText(String.format(getResources().getString(R.string.point_text),++num));
+        }
+        else {
+            Toast.makeText(MainActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+            mPointText.setText(String.format(getResources().getString(R.string.point_text),--num));
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mNumOneButton = (Button) findViewById(R.id.num_1);
-        mNumOneButton.setText(String.valueOf((int) (Math.random() * 100)));
-        mNumTwoButton = (Button) findViewById(R.id.num_2);
-        mNumTwoButton.setText(String.valueOf((int) (Math.random() * 100)));
-        mPointText = (TextView) findViewById(R.id.text_1);
-        mPointText.setText("Your point:"+String.valueOf(num));
+        mNumOneButton = findViewById(R.id.num_1);
+        mNumTwoButton = findViewById(R.id.num_2);
+        mPointText = findViewById(R.id.text_1);
+        mPointText.setText(String.format(getResources().getString(R.string.point_text),num));
+        randomSum();
         mNumOneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if( Integer.parseInt(mNumOneButton.getText().toString()) >= Integer.parseInt(mNumTwoButton.getText().toString())){
-                    Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
-                    mPointText.setText("Your point:"+String.valueOf(++num));
-                }
-                else {
-                    Toast.makeText(MainActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
-                    mPointText.setText("Your point:"+String.valueOf(--num));
-                }
-                mNumOneButton.setText(String.valueOf((int) (Math.random() * 100)));
-                mNumTwoButton.setText(String.valueOf((int) (Math.random() * 100)));
+                int num_1 = Integer.parseInt(mNumOneButton.getText().toString());
+                int num_2 = Integer.parseInt(mNumTwoButton.getText().toString());
+                toastShow(num_1,num_2);
+                randomSum();
             }
         });
         mNumTwoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(Integer.parseInt(mNumOneButton.getText().toString()) <= Integer.parseInt(mNumTwoButton.getText().toString())){
-                    Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
-                    mPointText.setText("Your point:"+String.valueOf(++num));
-                }
-                else {
-                    Toast.makeText(MainActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
-                    mPointText.setText("Your point:"+String.valueOf(--num));
-                }
-                mNumOneButton.setText(String.valueOf((int) (Math.random() * 100)));
-                mNumTwoButton.setText(String.valueOf((int) (Math.random() * 100)));
+                int num_1 = Integer.parseInt(mNumOneButton.getText().toString());
+                int num_2 = Integer.parseInt(mNumTwoButton.getText().toString());
+                toastShow(num_2,num_1);
+                randomSum();
             }
         });
     }
